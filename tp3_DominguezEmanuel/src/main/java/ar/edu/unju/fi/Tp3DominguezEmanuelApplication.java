6408cafe38922bbd5c19e453cba7d28c7f6cb6d0
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-05-11 11:36:28 -0300

    Se agrega la estructura del Proyecto

---
+++ tp3_DominguezEmanuel/src/main/java/ar/edu/unju/fi/Tp3DominguezEmanuelApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3DominguezEmanuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3DominguezEmanuelApplication.class, args);
+	}
+
+}
